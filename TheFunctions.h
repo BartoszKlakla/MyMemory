77b281f69313996bd58fd1a4c5eb92705f46d33a
Bartosz <Bartosz.Klakla@tlen.pl>
2015-10-12 20:06:41 +0200

    Functions begin

--- TheFunctions.h
+++ TheFunctions.h
@@ -5,19 +5,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* *Structures with datas* */
-typedef struct TheMemory
-{
+/* *Constant values* */
+#define MAX_BUFFER 50
+#define MEMORY_NAME "Mem_File.cmf"
 
+/* *Global variables* */
+FILE *InputFile;
+
+/* *Structures with datas* */
+typedef struct TheCodedMemory
+{
+	unsigned char *NameTable;
+	char InputBuffer[MAX_BUFFER];
+} TheMemory;
 
 /* *The functions used to connect with memory file* */
 
 /*Function:		OpenFile_Check - opens the output file and check for errors
  *Parameter:	InputFile - pointer to the file
  *Returns:		Status - variable which contains the boolean value of 1/0 if function worked correctly*/
-int OpenFile_Check();
+int OpenFile_Check(FILE *InputFile);
 
 /*Function:		WriteToFile - writes new memory coded into the file opened
  *Parameter:	InputFile - pointer to the file
  *Parameter:	Memory - structure with fields of the memory that have to be put*/
-void WriteToFile(FILE *InputFile, 
+void WriteToFile(FILE *InputFile, TheMemory Memory);
+
+#endif
