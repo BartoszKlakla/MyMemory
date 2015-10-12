7fa2df5580908c15dfb266abb4531581e09a9254
Bartosz <Bartosz.Klakla@tlen.pl>
2015-10-12 20:07:43 +0200

    Function file created

---
+++ TheFunctions.c
@@ -0,0 +1,20 @@
+#include "TheFunctions.h"
+
+int OpenFile_Check(FILE *InputFile)
+{
+	int Status = 0;						/*As for now, status is 0 - opening failed*/
+
+	InputFile = fopen(MEMORY_NAME, "w");
+	if(InputFile == NULL)
+	{
+		printf("Opening failed!\n");
+		return Status;
+	}
+	else
+	{
+		printf("File succesfully opened... Ready to write...\n");
+		Status++;
+		return Status;
+	}
+}
+
